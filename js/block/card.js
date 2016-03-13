1b62fac37e7f8b7025df7a7b1e7b560d18a29c5b
MarcusLeturiondo <mv.leturiondo@gmail.com>
2016-03-13 15:09:52 -0300

    upJS-v1
    Add colors

--- js/block/card.js
+++ js/block/card.js
@@ -1,7 +1,19 @@
-var $cardColors= document.querySelectorAll('.card-colors');
+var $cards = document.querySelectorAll('.card');
+var $cardColors = document.querySelectorAll('.card-colors');
 
-for (var i = 0; i < $cardColors.length; i++) {
-  $cardColors[i].addEventListener('click', function(){
-      alert('voce clicou');
-});
+for (var i = 0; i < $cards.length; i++) {
+  $cards[i].addEventListener('click', function(event){
+    var $this = event.target;
+
+    if($this.classList.contains('card-colors')){
+    var $card = this;
+
+    $card.dataset.color = $this.dataset.color;
+
+    for (var position = 0; position < $cardColors.length; position++) {
+      $cardColors[position].classList.remove('isActive');
+    }
+    $this.classList.add('isActive');
+  }
+  });
 }
